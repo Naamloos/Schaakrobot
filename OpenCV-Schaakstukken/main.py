b4b2e1b0161330b7968cb85a04ea11ca309bc921
Ryan de Jonge <naamloosdt@gmail.com>
2019-11-18 14:14:53 +0100

    Move PNG files

--- OpenCV-Schaakstukken/main.py
+++ OpenCV-Schaakstukken/main.py
@@ -79,21 +79,24 @@ def divide_hor_vert(lines):
     return hor, vert
 
 
-def get_blocks(intersects, gridsize):
+def get_pointgrid(intersects, gridsize):
     tlist = list(intersects)
-    tlist.sort(key=lambda i: i[1])
+    tlist = list(dict.fromkeys(tlist))
+    tlist.sort(key=lambda s: s[0])
 
     gridwidth = gridsize[0]
     gridheight = gridsize[1]
 
-    grid = []
+    grid = list()
 
     for i in range(0, gridwidth):
-        grid[i] = []
+        arr = []
         for j in range(0, gridheight):
-            grid[i][j] = intersects
+            arr.append(intersects[i + (gridheight * j)])
+        arr.sort(key=lambda s: s[1])
+        grid.append(arr)
 
-    return tlist
+    return grid
 
 
 def drawlines(lines, color, img):
@@ -118,68 +121,21 @@ def filter_highest(hor, vert):
     return returnhor, returnvert
 
 
-# https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/
-def order_points(pts):
-    # initialzie a list of coordinates that will be ordered
-    # such that the first entry in the list is the top-left,
-    # the second entry is the top-right, the third is the
-    # bottom-right, and the fourth is the bottom-left
-    rect = np.zeros((4, 2), dtype="float32")
+def get_all_blocks(pointgrid, img):
+    blocs = []
 
-    # the top-left point will have the smallest sum, whereas
-    # the bottom-right point will have the largest sum
-    s = pts.sum(axis=1)
-    rect[0] = pts[np.argmin(s)]
-    rect[2] = pts[np.argmax(s)]
+    pointgrid.sort(key=lambda l: l[0][1])
+    for i in range(0, len(pointgrid) - 1, 1):
+        pointgrid[i].sort(key=lambda l: l[0])
+        for j in range(0, len(pointgrid[i]) - 1, 1):
+            x1 = int(pointgrid[i][j][0])
+            y1 = int(pointgrid[i][j][1])
+            x2 = int(pointgrid[i+1][j+1][0])
+            y2 = int(pointgrid[i+1][j+1][1])
+            blocs.append(img[y1:y2+1, x1:x2+1])
 
-    # now, compute the difference between the points, the
-    # top-right point will have the smallest difference,
-    # whereas the bottom-left will have the largest difference
-    diff = np.diff(pts, axis=1)
-    rect[1] = pts[np.argmin(diff)]
-    rect[3] = pts[np.argmax(diff)]
+    return blocs;
 
-    # return the ordered coordinates
-    return rect
-
-
-def four_point_transform(image, pts):
-    # obtain a consistent order of the points and unpack them
-    # individually
-    rect = order_points(pts)
-    (tl, tr, br, bl) = rect
-
-    # compute the width of the new image, which will be the
-    # maximum distance between bottom-right and bottom-left
-    # x-coordiates or the top-right and top-left x-coordinates
-    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-    maxWidth = max(int(widthA), int(widthB))
-
-    # compute the height of the new image, which will be the
-    # maximum distance between the top-right and bottom-right
-    # y-coordinates or the top-left and bottom-left y-coordinates
-    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-    maxHeight = max(int(heightA), int(heightB))
-
-    # now that we have the dimensions of the new image, construct
-    # the set of destination points to obtain a "birds eye view",
-    # (i.e. top-down view) of the image, again specifying points
-    # in the top-left, top-right, bottom-right, and bottom-left
-    # order
-    dst = np.array([
-        [0, 0],
-        [maxWidth - 1, 0],
-        [maxWidth - 1, maxHeight - 1],
-        [0, maxHeight - 1]], dtype="float32")
-
-    # compute the perspective transform matrix and then apply it
-    M = cv.getPerspectiveTransform(rect, dst)
-    warped = cv.warpPerspective(image, M, (maxWidth, maxHeight))
-
-    # return the warped image
-    return warped
 
 # End functions
 
@@ -214,7 +170,10 @@ for i in intersects:
     if i[0] != float('inf') or i[1] != float('inf'):
         cv.circle(imgcpy, (int(i[0]), int(i[1])), 5, (0,255,0))
 
-blocks = get_blocks(intersects, (8,8))
+# get points in 2 dimensional array
+grid = get_pointgrid(intersects, (9,9))
+
+blocks = get_all_blocks(grid, img)
 
 # print all img versions
 cv.imshow('Original Image', img)
